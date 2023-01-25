9099dc67871123481e50c25759235e925cead954
seem <mwlorgat@gmail.com>
2023-01-25 09:45:28 +0200

    Initial Commit

---
+++ NSTableInSwiftUI/NSTableInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NSTableInSwiftUIApp.swift
+//  NSTableInSwiftUI
+//
+//  Created by Wasim Lorgat on 2023/01/25.
+//
+
+import SwiftUI
+
+@main
+struct NSTableInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
